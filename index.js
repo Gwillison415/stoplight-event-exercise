61ae99b07f513dc823fae8e4b3c5622027a9da30
Grant Willison <emailgdw@gmail.com>
2017-04-29 16:48:04 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 (function() {
   'use strict';
 
-  // YOUR CODE HERE
-})();
+  document.getElementById("goButton").addEventListener("mousedown", fireGreenLight);
+
+  document.getElementById("slowButton").addEventListener("mousedown", fireYellowLight);
+
+  document.getElementById("stopButton").addEventListener("mousedown", fireRedLight);
+
+  function fireRedLight() {
+    //console.log("red fired");  //must find out why fires twice per mousedown
+       document.getElementById('stopLight').style.backgroundColor = "red";
+    }
+  function fireYellowLight() {
+      document.getElementById('slowLight').style.backgroundColor = "yellow";
+  }
+  function fireGreenLight() {
+      document.getElementById('goLight').style.backgroundColor = "green";
+  }
+  // test in console whether or not a particular event was fired
+  let button = document.querySelectorAll('.button');
+  for (let i = 0; i < button.length; i++) {
+    button[i].addEventListener('mousedown', function(event) {
+      console.log(button[i].id, "event fired");
+    })
+  }
+}
+
+)();
